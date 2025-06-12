e657376c6cad58984cc36a1d5dcf68a9394306ad
Kevin <kevin@example.com>
2025-06-12 22:33:51 +0700

    Initial commit

---
+++ js.ts
@@ -0,0 +1,21 @@
+// typography.js (opsional jika ingin pisah file)
+export const size = {
+  displayLarge: '32px',
+  displayMedium: '26px',
+  displaySmall: '20px',
+  heading: '18px',
+  subheading: '16px',
+  body: '17px',
+  caption: '15px',
+};
+
+export const weight = {
+  thin: 100,
+  normal: 300,
+  bold: 700,
+};
+
+export const type = {
+  light: 'Roboto-Light',
+  medium: 'Roboto-Medium',
+};
